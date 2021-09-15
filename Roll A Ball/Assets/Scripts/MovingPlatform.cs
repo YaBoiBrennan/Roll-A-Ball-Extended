b6eeb6d302bdf78587b70adfaafcff0c79cfce9d
Brennan <brennanschlittler@gmail.com>
2021-09-14 23:10:24 -0500

    Initial Commit

---
+++ Roll A Ball/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    Transform platform;
+
+    [SerializeField]
+    Transform startTransform;
+
+    [SerializeField]
+    Transform endTransform;
+
+    void onDrawGizmos()
+    {
+        Gizmos.DrawWireCube(startTransform.position, platform.localScale);
+    }
+
+
+		
+	}
+
